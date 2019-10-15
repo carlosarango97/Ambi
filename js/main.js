c6617f2a00d1d1c808251e3d21b51d173a54eb5b
carlosarango97 <47753930+carlosarango97@users.noreply.github.com>
2019-10-14 19:13:01 -0500

    Repairs
    I implemented check answer function also I did the translate of plants-section.

--- js/main.js
+++ js/main.js
@@ -1,31 +1,51 @@
 const db=firebase['firestore']();
+var correct = 2;
 
 function changePage(idIn, idOut){
+    if(idIn=="trivia-section"){
+        triviaStarter();
+    }
     document.getElementById(idIn).classList.remove("invisible");
     document.getElementById(idOut).className += " invisible";
 }
 
 function triviaStarter(){
-    var preguntas = 4;
-    var i = parseInt(Math.random() * (preguntas - 1) + 1);
+    var questions = 4;
+    var i = parseInt(Math.random() * (questions - 1) + 1);
     db.collection("Preguntas").doc(i.toString()).get().then(snap => {
         document.getElementById("question").innerHTML = snap.data().Pregunta;
-        var j = parseInt(Math.random() * 4 + 1);
-        document.getElementById("span_answer" + j).innerHTML = snap.data().Correcta;
-        var contador = 1;
+        correct = parseInt(Math.random() * 4 + 1);
+        document.getElementById("span_answer" + correct).innerHTML = snap.data().Correcta;
+        var count = 1;
         for(var k=1; k<=4; k++){
-            if(k!=j){
-                if(contador==1){
+            if(k!=correct){
+                if(count==1){
                     document.getElementById("span_answer" + k).innerHTML = snap.data().Incorrecta1;
-                    contador++;
-                }else if(contador==2){
+                    count++;
+                }else if(count==2){
                     document.getElementById("span_answer" + k).innerHTML = snap.data().Incorrecta2;
-                    contador++;
-                }else if(contador==3){
+                    count++;
+                }else if(count==3){
                     document.getElementById("span_answer" + k).innerHTML = snap.data().Incorrecta3;
-                    contador++;
+                    count++;
                 }
             }
         }
     });
+}
+
+function checkAnswer(answer){
+    if(answer==correct){
+        correctAnswer();
+    }else{
+        wrongAnswer();
+    }
+}
+
+function wrongAnswer(){
+    alert("Wrong answer!");
+}
+
+function correctAnswer(){
+    changePage('plants-section','trivia-section');
 }
